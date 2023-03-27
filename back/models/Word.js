1170738eb8ac7602c352f29abee802fbce1b03c9
Georgi Karchev <georgi.karchev@gmail.com>
2023-03-27 11:11:15 +0300

    Add Word model

---
+++ back/models/Word.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const wordSchema = new mongoose.Schema({
+  word: {
+    type: String,
+    required: [true, "Word is required"],
+  },
+  type: {
+    type: String,
+    default: 'noun',
+    enum: ["noun", "verb", "adjective", "adverb", "punctuation"],
+  },
+  isPlural: {
+    type: Boolean,
+    default: false,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    maxLength: [160, "Description can be no longer that 160 characters"],
+  },
+  notes: {
+    type: String,
+    maxLength: [160, "Notes can be no longer that 160 characters"],
+  },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    required: true,
+  },
+  modified: {
+    type: Date,
+  },
+});
+
+const Word = mongoose.model("Word", wordSchema);
+
+module.exports = Word;
