846d80e21caf2d568e4d70bd493a6cf22baa348c
Georgi Karchev <georgi.karchev@gmail.com>
2023-03-27 11:11:38 +0300

    Add text model

---
+++ back/models/Text.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const textSchema = new mongoose.Schema({
+  expressions: [
+    {
+      expression: {
+        type: mongoose.Types.ObjectId,
+        ref: "Expression",
+      },
+    },
+  ],
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    maxLength: [160, "Description can be no longer that 160 characters"],
+  },
+  notes: {
+    type: String,
+    maxLength: [160, "Notes can be no longer that 160 characters"],
+  },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    required: true,
+  },
+  modified: {
+    type: Date,
+  },
+});
+
+const Text = mongoose.model("Text", textSchema);
+
+module.exports = Text;
