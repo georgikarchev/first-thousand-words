75881c9dd5929c4fbbf3588bb052d2e097667efc
Georgi Karchev <georgi.karchev@gmail.com>
2023-04-01 23:28:33 +0300

    Add expressionController

---
+++ back/controllers/expressionController.js
@@ -0,0 +1,70 @@
+const expressionService = require("../services/expressionService");
+const { DEFAULT_DB_FETCH_LIMIT } = require("../constants");
+
+const { getErrorMessage } = require("../utils/errorUtils");
+const { removeEmptyAttributes } = require("../utils/jsUtils");
+
+exports.getMany = async (req, res) => {
+  const query = { ...req.query };
+  query.skip = query.skip ? query.skip : 0;
+  query.limit = query.limit ? query.limit : DEFAULT_DB_FETCH_LIMIT;
+  query.sortBy = query.sortBy ? query.sortBy : "name";
+  query.orderBy = query.orderBy ? query.orderBy : "asc";
+
+  try {
+    const result = await expressionService.getMany(query);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: getErrorMessage(error) });
+  }
+};
+
+exports.getOne = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const result = await expressionService.getById(id);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(404).json({ error: getErrorMessage(error) });
+  }
+};
+
+exports.createOne = async (req, res) => {
+  try {
+    const { words } = req.body;
+
+    if (!words) {
+      throw new Error("Bad input");
+    }
+
+    const result = await expressionService.create({words});
+
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: getErrorMessage(error) });
+  }
+};
+
+exports.deleteOne = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const result = await expressionService.deleteOne(id);
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(404).json({ error: getErrorMessage(error) });
+  }
+};
+
+exports.updateOne = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await expressionService.update(id, req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({ error: getErrorMessage(error) });
+  }
+};
