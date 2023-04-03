5512176b0efee8e623e0b251f363450eb84fd9cd
Georgi Karchev <georgi.karchev@gmail.com>
2023-04-03 16:49:06 +0300

    Add dialogueController and dialogueService

---
+++ back/services/dialogueService.js
@@ -0,0 +1,125 @@
+const Expression = require("../models/Expression");
+const Text = require("../models/Text");
+const Dialogue = require("../models/Dialogue");
+const { removeEmptyAttributes, isEmpty } = require("../utils/jsUtils");
+const { isValidMongoId } = require("../utils/validators");
+
+exports.getMany = async (query) => {
+  let qry = {};
+  let result = { data: {}, count: 0 };
+
+  result.count = await Dialogue.find({}).countDocuments();
+
+  qry.limit = query.limit ? query.limit : 20;
+  result.limit = qry.limit;
+
+  qry.skip = query.skip
+    ? query.skip < result.count
+      ? query.skip
+      : result.count
+    : 0;
+  result.skip = qry.skip;
+
+  qry.find = {};
+
+  if (query.creator) {
+    qry.find.creator = query.creator;
+  }
+
+  qry.active = query.active === false ? false : true;
+
+  qry.orderBy = query.orderBy === "desc" ? "desc" : "asc";
+
+  qry.sort = {};
+
+  if (query.sortBy) {
+    qry.sort[query.sortBy] = qry.orderBy;
+  } else {
+    qry.sort = { name: qry.orderBy };
+  }
+
+  result.data = await Dialogue.find(qry.find)
+    .sort(qry.sort)
+    .skip(qry.skip)
+    .limit(qry.limit)
+    .lean();
+  return result;
+};
+
+exports.getById = async (id) => {
+  if (!isValidMongoId(id)) {
+    throw new Error("Invalid dialogue id");
+  }
+
+  const dialogue = await Dialogue.findById(id).lean();
+
+  if (!dialogue) {
+    throw new Error("Dialogue does not exist.");
+  }
+
+  return dialogue;
+};
+
+exports.create = async (data, type) => {
+  const { expressions } = data;
+
+  const newDialogue = { expressions: expressions };
+  if (type != null) {
+    newDialogue.type = type;
+  }
+  newDialogue.created = new Date();
+
+  return await Dialogue.create(newDialogue);
+};
+
+exports.update = async (id, data) => {
+  if (!isValidMongoId(id)) {
+    throw new Error("Invalid dialogue id");
+  }
+  
+  const dialogue = await Dialogue.findById(id);
+  
+  if (!dialogue) {
+    throw new Error("Dialogue does not exist");
+  }
+  
+  const newData = removeEmptyAttributes(data);
+  
+  if (newData.language) {
+    dialogue.language = newData.language;
+  }
+
+  if (newData.expressions) {
+    dialogue.expressions = newData.expressions;
+  }
+
+  if (newData.published) {
+    dialogue.published = newData.published;
+  }
+
+  if (newData.description) {
+    dialogue.description = newData.description;
+  }
+
+  if (newData.notes) {
+    dialogue.notes = newData.notes;
+  }
+
+  if (!isEmpty(newData)) {
+    dialogue.modified = new Date();
+    return await dialogue.save();
+  }
+
+  throw new Error("Bad input");
+};
+
+exports.deleteOne = async (id) => {
+  // 1. check if dialogue exists
+  const dialogue = await Dialogue.findById(id);
+  
+  if (!dialogue) {
+    throw new Error("Dialogue does not exist.");
+  }
+
+  return await Dialogue.findByIdAndDelete(id).lean();
+};
