655613627683a37d568ea2bb3998ae44cd2f25fa
Georgi Karchev <georgi.karchev@gmail.com>
2023-04-11 19:09:32 +0300

    Add basic state and test conditional layout

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,31 +4,34 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import { ThemeOptions } from '@mui/material/styles';
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Badge from "@mui/material/Badge";
+import { ThemeOptions } from "@mui/material/styles";
 
 import TextCard from "./components/texts/text-card/TextCard";
 
 import "./App.scss";
-
+import { useEffect, useState } from "react";
+import useScroll from "./hooks/useScroll";
 
 const themeOptions = {
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: '#D36B00',
+      main: "#D36B00",
     },
     secondary: {
-      main: '#f7e06a',
+      main: "#f7e06a",
     },
     background: {
-      default: '#F1EFDC',
-      paper: '#E6D2AA',
+      default: "#F1EFDC",
+      paper: "#E6D2AA",
     },
     text: {
-      primary: '#D36B00',
-      secondary: '#f7e06a',
-      disabled: 'rgba(197,137,64,0.2)',
-      hint: '#ffa242',
+      primary: "#D36B00",
+      secondary: "#f7e06a",
+      disabled: "rgba(197,137,64,0.2)",
+      hint: "#ffa242",
     },
   },
 };
@@ -97,38 +100,128 @@ function App() {
     },
   ];
 
-  const appState = {
+  const appStateDefault = {
     dummyText: dummyExpressions,
+    scrollProgress: 0,
+    isTop: 0,
   };
+  // ------ dev only code above
+  const [appState, setAppState] = useState(appStateDefault);
+  const scrollPosition = useScroll();
 
-  const testExpressions = Array("one", "two", "three");
-  const testString = "My String";
+  useEffect(()=> {
+    setAppState(state => ({...state, isTop: scrollPosition > 50}))
+  },[scrollPosition]);
+
+  function shrinkHeader() {
+    window.scrollTo(0, 0);
+    setAppState((state) => {
+      return { ...state, isTop: false };
+    });
+  }
+
+  function expandHeader() {
+    setAppState((state) => {
+      return { ...state, isTop: true };
+    });
+  }
+  
+
+  function MainMenu({ params }) {
+    return (
+      <nav>
+        <ButtonGroup
+          variant="text"
+          size="large"
+          aria-label="large primary button group"
+        >
+          <Badge badgeContent={4} color="secondary">
+            <Button>One</Button>
+          </Badge>
+          <Badge badgeContent={4} color="secondary">
+            <Button>Two</Button>
+          </Badge>
+          <Badge badgeContent={4} color="secondary">
+            <Button>Three</Button>
+          </Badge>
+        </ButtonGroup>
+      </nav>
+    );
+  }
 
   return (
-    <div className="App">
+    <div className={`App${appState.isTop ? " dashboard" : ""}`}>
       <header className="App-header">
         <h1>First thousand words</h1>
       </header>
       <main>
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={2}>
-            <Grid item xs={12} md={4} className="stats" style={{paddingLeft: "unset"}}>
-              1000 <span>words</span>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              className="stats"
+              style={{ paddingLeft: "unset" }}
+            >
+              <Badge badgeContent={100} max={9999} color="secondary">
+                <Button size="large">words</Button>
+              </Badge>
             </Grid>
-            <Grid item xs={12} md={4} className="stats" style={{paddingLeft: "unset"}}>
-              120 <span>expressions</span>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              className="stats"
+              style={{ paddingLeft: "unset" }}
+            >
+              <Badge badgeContent={120} max={999} color="secondary">
+                <Button size="large">expressions</Button>
+              </Badge>
             </Grid>
-            <Grid item xs={12} md={4} className="stats" style={{paddingLeft: "unset"}}>
-              20 <span>texts</span>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              className="stats"
+              style={{ paddingLeft: "unset" }}
+            >
+              <Badge badgeContent={20} max={99} color="secondary">
+                <Button size="large">texts</Button>
+              </Badge>
             </Grid>
-            <Grid item xs={12} md={6} style={{textAlign: "center"}}>
-              <Button variant="outlined">New text</Button>
+            <Grid item xs={12} md={6} style={{ textAlign: "center" }}>
+              <Button variant="outlined" onClick={shrinkHeader}>
+                New text
+              </Button>
             </Grid>
-            <Grid item xs={12} md={6} style={{textAlign: "center"}}>
-              <Button variant="outlined">New Dialogue</Button>
+            <Grid item xs={12} md={6} style={{ textAlign: "center" }}>
+              <Button variant="outlined" onClick={expandHeader}>
+                New Dialogue
+              </Button>
             </Grid>
           </Grid>
         </Box>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+        <p>bla</p>
+
       </main>
     </div>
   );
